65d5da034b2aa055db824e41912e3051ffa3c292
purwar1997 <shubhampurwar511@gmail.com>
2025-07-09 11:47:43 +0530

    Update ESLint config

--- eslint.config.js
+++ eslint.config.js
@@ -2,7 +2,7 @@ import js from '@eslint/js';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
 import pluginReact from 'eslint-plugin-react';
-import { defineConfig } from 'eslint/config';
+import { defineConfig, globalIgnores } from 'eslint/config';
 
 export default defineConfig([
   {
@@ -30,4 +30,5 @@ export default defineConfig([
     },
   },
   tseslint.configs.recommended,
+  globalIgnores(['node_modules', 'build', '.react-router']),
 ]);
